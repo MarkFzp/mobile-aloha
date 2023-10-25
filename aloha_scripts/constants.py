10c726e5ecbc142721962150c72533c7ec3c270d
Zipeng Fu <zipengfu@outlook.com>
2023-10-25 16:12:50 -0400

    aloha_mobile_grasp_pen_all

--- aloha_scripts/constants.py
+++ aloha_scripts/constants.py
@@ -15,6 +15,18 @@ TASK_CONFIGS = {
         'episode_len': 500,
         'camera_names': ['cam_high', 'cam_left_wrist', 'cam_right_wrist']
     },
+    'aloha_mobile_grasp_pen_diverse':{
+        'dataset_dir': DATA_DIR + '/aloha_mobile_grasp_pen_diverse',
+        'num_episodes': 50,
+        'episode_len': 500,
+        'camera_names': ['cam_high', 'cam_left_wrist', 'cam_right_wrist']
+    },
+    'aloha_mobile_grasp_pen_all':{
+        'dataset_dir': DATA_DIR + '/aloha_mobile_grasp_pen_all',
+        'num_episodes': 100,
+        'episode_len': 500,
+        'camera_names': ['cam_high', 'cam_left_wrist', 'cam_right_wrist']
+    },
 }
 
 ### ALOHA fixed constants
